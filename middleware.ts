14009573d6e532140581960c276b5769a728469c
richwilks <rich_wilks@sky.com>
2026-07-27 22:14:16 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -29,9 +29,11 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  const isAuthPage =
+    const isAuthPage =
     request.nextUrl.pathname.startsWith('/login') ||
-    request.nextUrl.pathname.startsWith('/signup')
+    request.nextUrl.pathname.startsWith('/signup') ||
+    request.nextUrl.pathname.startsWith('/forgot-password') ||
+    request.nextUrl.pathname.startsWith('/reset-password')
 
   if (!user && !isAuthPage) {
     const url = request.nextUrl.clone()
