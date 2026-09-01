dc155d25467e5c45caa154203dae353b04a8e9dc
richwilks <rich_wilks@sky.com>
2026-09-01 15:35:38 +0100

    Create handler.py

---
+++ handler.py
@@ -0,0 +1,12 @@
+--- Starting Serverless Worker |  Version 1.7.9 ---
+INFO   | Using test_input.json as job input.
+DEBUG  | Retrieved local job: {'input': {'prompt': 'Hey there!'}, 'id': 'local_test'}
+INFO   | local_test | Started.
+Worker Start
+Received prompt: Hey there!
+Sleeping for 0 seconds...
+DEBUG  | local_test | Handler output: Hey there!
+DEBUG  | local_test | run_job return: {'output': 'Hey there!'}
+INFO   | Job local_test completed successfully.
+INFO   | Job result: {'output': 'Hey there!'}
+INFO   | Local testing complete, exiting.
